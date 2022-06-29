b2500bcfd6ddd069500759b250d921204eb1447d
Priyanka <priyankahansi95@gmail.com>
2022-06-29 23:11:13 +0530

    added:Employee Payroll DTO

--- src/main/java/com/bridgelabz/springemployeepayroll/service/IEmployeePayrollService.java
+++ src/main/java/com/bridgelabz/springemployeepayroll/service/IEmployeePayrollService.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.springemployeepayroll.service;
 
+import com.bridgelabz.springemployeepayroll.Dto.EmployeeDTO;
 import com.bridgelabz.springemployeepayroll.Entity.Employee;
 
 import java.util.List;
@@ -7,14 +8,14 @@ import java.util.List;
 public interface IEmployeePayrollService {
     String getMessage();
 
-    String AddEmployee(Employee employee);
-
     Employee getEmployeeDetails(int getId);
 
     List<Employee> getListOfEmployees();
 
     void deleteEmployee(int id);
 
-    Employee updateEmployee(int getId, Employee employee);
+    Employee updateEmployee(int getId, EmployeeDTO employeedto);
+
+    String AddEmployee(EmployeeDTO employeeDTO);
 
 }
