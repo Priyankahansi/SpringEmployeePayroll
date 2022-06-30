7132fb557c3ca2a44d9efe694a06513e8b09e29e
Priyanka <priyankahansi95@gmail.com>
2022-07-01 00:27:54 +0530

    added:Lombok,Validation,DataBaseSetUP,Exception Layer

--- src/main/java/com/bridgelabz/springemployeepayroll/Entity/Employee.java
+++ src/main/java/com/bridgelabz/springemployeepayroll/Entity/Employee.java
@@ -1,20 +1,28 @@
 package com.bridgelabz.springemployeepayroll.Entity;
 
 import com.bridgelabz.springemployeepayroll.Dto.EmployeeDTO;
+import jdk.jfr.Name;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
+@ToString
 public class Employee {
 
     @Id
     @GeneratedValue
     private int id;
     private String gender;
-
     private String employeeName;
     private String department;
     private long salary;
@@ -22,22 +30,6 @@ public class Employee {
     private LocalDate Date;
     private String profilePic;
     private String note;
-
-    public Employee() {
-    }
-
-    public Employee(int id, String gender, String employeeName, String department, long salary,
-                    String email, LocalDate date, String profilePic, String note) {
-        this.id = id;
-        this.gender = gender;
-        this.employeeName = employeeName;
-        this.department = department;
-        this.salary = salary;
-        this.email = email;
-        Date = date;
-        this.profilePic = profilePic;
-        this.note = note;
-    }
 
     public Employee(EmployeeDTO employeeDto) {
         this.gender = employeeDto.getGender();
@@ -48,92 +40,5 @@ public class Employee {
         Date = employeeDto.getDate();
         this.profilePic = employeeDto.getProfilePic();
         this.note = employeeDto.getNote();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDate() {
-        return Date;
-    }
-
-    public void setDate(LocalDate date) {
-        Date = date;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", gender='" + gender + '\'' +
-                ", employeeName='" + employeeName + '\'' +
-                ", department='" + department + '\'' +
-                ", salary=" + salary +
-                ", email='" + email + '\'' +
-                ", Date=" + Date +
-                ", profilePic='" + profilePic + '\'' +
-                ", note='" + note + '\'' +
-                '}';
     }
 }
---
+++ src/main/java/com/bridgelabz/springemployeepayroll/exceptions/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.springemployeepayroll.exceptions;
+
+public class EmployeePayrollException extends RuntimeException {
+
+    public EmployeePayrollException(String message) {
+        super(message);
+    }
+}
